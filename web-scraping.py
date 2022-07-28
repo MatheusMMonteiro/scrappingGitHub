36de977a55897c2c698586c70bfe8dd1a8c05ed0
matheus <matheus.malmeida7@gmail.com>
2022-07-27 23:39:57 -0300

    adding contributions and organization and date

--- web-scraping.py
+++ web-scraping.py
@@ -1,13 +1,11 @@
-import requests #lib para requisição
-from bs4 import BeautifulSoup #lib de extração de dados
+import requests 
+from bs4 import BeautifulSoup
 import time
 
-link = input("Digite seu usuário do GitHub: ")
+link = input("\nDigite seu user do GitHub: ")
 link = "https://github.com/" + link
 
 page = requests.get(link)
-
-#print(page.status_code)
 
 if(page.status_code == 404):
     print("Usuário não existe!")
@@ -19,51 +17,45 @@ soup = BeautifulSoup(page.content, 'html.parser') #instância o BeatSoup na vari
 
 #print(soup.prettify()) #conteúdo de uma página
 
-name = soup.find('span', class_='p-name').get_text().strip() #retorna as string da tag na variável
+name = soup.find('span', class_='p-name').get_text().strip() #retorna a string da tag na variável
 
 userName = soup.find('span', class_='p-nickname').get_text().strip()
-organization = soup.find('span', class_='p-org')
+job = soup.find('span', class_='p-org')
 local = soup.find('span', class_='p-label')
+organization = soup.findAll(attrs={'data-hovercard-type':'organization', 'class':'avatar-group-item'})
+contributions = soup.find('div', class_='js-yearly-contributions')
+if(contributions):
+    contributions = contributions.find('h2', class_='f4')
+date = soup.findAll('a', class_='js-year-link')
 
-# print("\nAguarde...")
-# time.sleep(3)
-# print("\nTrazendo informações...")
-# time.sleep(1.5)
 
 print("\nNome:",name)
-print("\nNome de usuário:", userName)
+print("\nUser:", userName)
 
 # check if user has github pro
 pro = soup.find('span', class_='Label Label--purple text-uppercase')
 if pro:
-    print("\nUsuário é PRO")
+    print("\nUser é PRO")
 else:
-    print("\nUsuário não é PRO")
+    print("\nUser não é PRO")
 
-
-# check if user has github pro
-pro = soup.find('span', class_='Label Label--purple text-uppercase')
-if pro:
-    print("\nUsuário é PRO")
-else:
-    print("\nUsuário não é PRO")
-
-
-
-if(organization != None):
-   organization = organization.get_text().strip()
-   print("\nOrganização:", organization)
-if(local != None):
+if(job):
+   job = job.get_text().strip()
+   print("\nEmpresa:", job)
+if(local):
     local = local.get_text().strip()
     print("\nLocalidade:", local)
+if(contributions):
+    contributions = contributions.get_text().strip().split()
+    print("\n"+contributions[0] + " contribuições\n")
+if(organization):
+    print("Organizações:")
+    for org in organization:
+        print(" - "+org['aria-label'].strip())
+if(date):
+    date = date[-1].get_text().strip()
+    print("\nContribuindo desde "+ date + " XD")
 
-contributions = soup.find('div', class_='js-yearly-contributions')
-
-contributions = contributions.find('h2', class_='f4')
-
-contributions = contributions.get_text().strip().split()
-
-print("\n"+contributions[0] + " contribuições\n")
 
 
 
